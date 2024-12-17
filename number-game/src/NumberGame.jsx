0392083542759576806f103ee4aaf7722fd2d512
Franklin Kong <franklinkong981@gmail.com>
2024-12-17 12:54:11 -0800

    Finished demo

--- number-game/src/NumberGame.jsx
+++ number-game/src/NumberGame.jsx
@@ -4,12 +4,25 @@ const NumberGame = (props) => {
   const genRandom = () => Math.floor(Math.random() * 10) + 1;
   const [guess, setGuess] = useState(genRandom());
   const [target, setTarget] = useState(genRandom());
+  const [guessCount, setGuessCount] = useState(0);
+
   const isWinner = target === guess;
+  const restart = () => {
+    setTarget(genRandom());
+    setGuess(0);
+    setGuessCount(0);
+  };
+  const makeGuess = () => {
+    setGuess(genRandom());
+    setGuessCount(guessCount + 1);
+  }
   return (
-    <div>
+    <div className="NumberGame">
       <h1>Target Num: {target}</h1>
       <h1 style={{color: isWinner ? 'green' : 'red'}}>Your Guess: {guess}</h1>
-      {isWinner ? null : <button onClick={() => setGuess(genRandom())}>Generate Num</button>}
+      <h4>Guess #{guessCount}</h4>
+      {!isWinner && <button onClick={makeGuess}>Generate Num</button>}
+      <button onClick={restart}>New Game</button>
     </div>
   );
 };
